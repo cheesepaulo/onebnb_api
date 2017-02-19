60b125194cc9e89e7e574edc331d6e5e305eee1e
Paulo Cesar <pcdsjr@gmail.com>
2017-02-18 19:31:54 -0500

    listing messages and make some tests

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::TalksController < ApplicationController
   before_action :authenticate_api_v1_user!
+  before_action :set_api_v1_talk, only: [:messages]
+  before_action :is_talk_participant?, only: [:messages]
 
   def index
     page = (params[:page])? params[:page] : 1
-    
+
     @talks = Talk.includes(:messages)
                   .joins(:property)
                   .where(["properties.user_id = ? or talks.user_id = ?", current_api_v1_user.id, current_api_v1_user.id])
@@ -11,4 +13,20 @@ class Api::V1::TalksController < ApplicationController
                   .order("messages.created_at DESC")
                   .paginate(:page => page, :per_page => 8)
   end
+
+  def messages  
+  end
+
+
+  private
+    def set_api_v1_talk
+      @talk = Talk.find(params['id'])
+    end
+
+    def is_talk_participant?
+      if @talk.user != current_api_v1_user and @talk.property.user != current_api_v1_user
+        render json: {message: 'unauthorized'}, status: 401 and return
+      end
+    end
+
 end
